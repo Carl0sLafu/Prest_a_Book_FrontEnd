1e9e937cbbc90faf766a9b99d1225f581b44dd96
AlbertoJiLo <alberto_tgn_14@hotmail.com>
2022-12-10 12:09:12 +0100

    Conexion a api comentado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-// import { HttpClient } from '@angular/common/http'
-=======
->>>>>>> c77d7e7de638de2216604dac6c1bb1684767f692
+import { HttpClient } from '@angular/common/http'
+
 
 
 @Component({
@@ -10,16 +8,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-<<<<<<< HEAD
-export class AppComponent /*implements OnInit*/ {
-  title = 'prest-a-book';
 
-  // books: any = null;
-  // users: any = null;
-  // drawers: any = null;
-  // editorials: any = null;
-  // authors: any = null;
-=======
 export class AppComponent {
   title = 'prest-a-book';
 
@@ -28,32 +17,27 @@ export class AppComponent {
   drawers: any = null;
   editorials: any = null;
   authors: any = null;
->>>>>>> c77d7e7de638de2216604dac6c1bb1684767f692
 
-  // constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){}
 
-  // ngOnInit(): void{
-  //   this.http.get('https://prestabookbackend-production.up.railway.app/api/books').subscribe
-  //   (res => this.books = res);
+  ngOnInit(): void{
+   this.http.get('https://prestabookbackend-production.up.railway.app/api/books').subscribe
+   (res => this.books = res);
 
-  //   this.http.get('https://prestabookbackend-production.up.railway.app/api/users').subscribe
-  //   (res => this.users = res);
+   this.http.get('https://prestabookbackend-production.up.railway.app/api/users').subscribe
+   (res => this.users = res);
 
-  //   this.http.get('https://prestabookbackend-production.up.railway.app/api/drawers').subscribe
-  //   (res => this.drawers = res);
+   this.http.get('https://prestabookbackend-production.up.railway.app/api/drawers').subscribe
+   (res => this.drawers = res);
     
-  //   this.http.get('https://prestabookbackend-production.up.railway.app/api/authors').subscribe
-  //   (res => this.authors = res);
+   this.http.get('https://prestabookbackend-production.up.railway.app/api/authors').subscribe
+   (res => this.authors = res);
 
-<<<<<<< HEAD
-  //   this.http.get('https://prestabookbackend-production.up.railway.app/api/editorials').subscribe
-  //   (res => this.editorials = res);
-  // }
-=======
+
     this.http.get('https://prestabookbackend-production.up.railway.app/api/editorials').subscribe
     (res => this.editorials = res);
   }*/
->>>>>>> c77d7e7de638de2216604dac6c1bb1684767f692
+
 
   
 
