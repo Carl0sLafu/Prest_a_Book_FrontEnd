f771553d4cc7f6a553d86a30d18f0d97e00189fc
Carl0sLafu <114726441+Carl0sLafu@users.noreply.github.com>
2022-12-10 09:06:07 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'prest-a-book';
 
   /*books: any = null;
