2da496398ab174209cc84b7d04f475fdaaab5028
Carl0sLafu <114726441+Carl0sLafu@users.noreply.github.com>
2022-12-10 09:47:29 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { BooksearchComponent } from './booksearch/booksearch.component';
