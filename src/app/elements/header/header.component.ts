25a3ce178aa6e5e97058d32fcf7a825f687502a1
Carl0sLafu <114726441+Carl0sLafu@users.noreply.github.com>
2022-12-15 09:55:16 +0100

    Add files via upload

--- src/app/elements/header/header.component.ts
+++ src/app/elements/header/header.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '../../_services/token-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  isLoged: boolean = false;
+
+  constructor (private tokenStorage: TokenStorageService) {}
+
+  ngOnInit(): void {
+    
+    if (this.tokenStorage.getToken()) {
+
+      this.isLoged = true;
+
+    } else {
+
+      this.isLoged = false;
+
+    }
+
+  }
+
+  logOut(): void {
+
+    this.tokenStorage.signOut();
+    window.location.reload();
+
+  }
 
 }
