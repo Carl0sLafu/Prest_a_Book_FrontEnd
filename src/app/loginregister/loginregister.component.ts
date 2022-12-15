ed4e7164b40d3cbca711cd0bfc618e4ecceb3bdc
Carl0sLafu <114726441+Carl0sLafu@users.noreply.github.com>
2022-12-15 09:54:40 +0100

    Add files via upload

--- src/app/loginregister/loginregister.component.ts
+++ src/app/loginregister/loginregister.component.ts
@@ -1,22 +1,75 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
   selector: 'app-loginregister',
   templateUrl: './loginregister.component.html',
   styleUrls: ['./loginregister.component.css']
 })
-export class LoginregisterComponent {
+export class LoginregisterComponent implements OnInit {
 
   register: boolean = false;
+  loginFormResults: any = {
 
-  constructor () {
+    username: null,
+    password: null
+
+  };
+
+  registerFormResults: any = {
+
+    username: null,
+    email: null,
+    password: null,
+    confirmedpassword: null
+
+  };
+
+  errorMessageLogin = '';
+
+  constructor (private authService: AuthService, private tokenStorage: TokenStorageService) {
+
+  }
+
+  ngOnInit(): void {
+    
+    if (this.tokenStorage.getToken()) {
+
+      window.location.assign("..");
+
+    }
+
+  }
+
+  onSubmitLogin(): void {
+
+    const {username, password} = this.loginFormResults;
+    this.authService.login(username, password).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(data);
+
+        this.reloadPage();
+      },
+      error => {
+
+        this.errorMessageLogin = error;
+
+      }
+    );
 
   }
 
   cambiarEstado() {
 
     this.register = this.register?false:true;
+
+  }
+
+  reloadPage(): void {
+
+    window.location.reload();
 
   }
 
