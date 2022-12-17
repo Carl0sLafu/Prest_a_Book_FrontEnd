9f074254ae49f596d02dc56dcb67f9793a9d717f
Carl0sLafu <114726441+Carl0sLafu@users.noreply.github.com>
2022-12-17 09:36:15 +0100

    Add files via upload

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Books } from '../models/books.model';
+import { BooksService } from '../services/books.service';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+
+  constructor(private BooksService:BooksService){}
+
+  books: Books[] = [];
+  count: number = 3;
+
+  ngOnInit():void {
+
+    this.BooksService.getAll().subscribe(result => this.books = result);
+
+  }
 
 }
