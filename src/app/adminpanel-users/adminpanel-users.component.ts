e04502b6ae6c331bc5fdeeab8560c3f94dd506b2
AlbertoJiLo <alberto_tgn_14@hotmail.com>
2022-12-17 18:58:22 +0100

    Cambio roles admin panel y cambio de fuente del main

--- src/app/adminpanel-users/adminpanel-users.component.ts
+++ src/app/adminpanel-users/adminpanel-users.component.ts
@@ -12,6 +12,13 @@ export class AdminpanelUsersComponent {
 
   usuarios:any = null
   estasSeguro: any = false;
+  rol:string = "";
+  nuevosDatosUsuario:any = {
+    id_role:{
+      id: null
+    }
+  };
+
 
   constructor(private users: UsersService){
     
@@ -25,10 +32,30 @@ export class AdminpanelUsersComponent {
   
   borrarUsuario(usuario_id:number){
     this.estasSeguro = window.confirm("¿Estás seguro?");
-    console.log(usuario_id)
+
     if(this.estasSeguro){
           this.users.delete(usuario_id).subscribe()
     }
+  }
 
+  desplegarRol(usuario:any){
+    if(usuario.id_role.id == 3){
+      this.estasSeguro = window.confirm
+      ("¿Quieres cambiar el rol de "+usuario.username+" a Colaborador?");
+      if(this.estasSeguro){
+          usuario.id_role.id = 2;
+          this.users.update(usuario.id,usuario).subscribe();
+      }
+    }else if(usuario.id_role.id == 2){
+      this.estasSeguro = window.confirm
+      ("¿Quieres cambiar el rol de "+usuario.username+" a User?");
+      if(this.estasSeguro){
+          usuario.id_role.id = 3;
+          this.users.update(usuario.id,usuario).subscribe();
+      }
+    }else if(usuario.id_role.id == 1){
+      window.alert("No puedes cambiar el rol de un administrador.");
+    }
+    
   }
 }
