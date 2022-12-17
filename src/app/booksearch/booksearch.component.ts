7594c15e80bae5d03b494fce9829e0f8d7e84dba
Carl0sLafu <114726441+Carl0sLafu@users.noreply.github.com>
2022-12-17 16:25:06 +0100

    Add files via upload

--- src/app/booksearch/booksearch.component.ts
+++ src/app/booksearch/booksearch.component.ts
@@ -12,9 +12,54 @@ export class BooksearchComponent implements OnInit{
   constructor(private BooksService:BooksService){}
 
   books?: Books[];
+  ordenarBy: number = 0;
 
   ngOnInit():void{
-    this.BooksService.getAll().subscribe(result => this.books = result);
+
+    this.orderBooks();
+
+  }
+
+  orderBooks() {
+
+    if (this.ordenarBy == 0) {
+
+      this.BooksService.getAll().subscribe(result => this.books = result.sort(function (a, b) {
+        if (a.title! > b.title!) {
+          return 1;
+        }
+        if (a.title! < b.title!) {
+          return -1;
+        }
+        return 0;
+      }));
+
+    } else if (this.ordenarBy == 1) {
+
+      this.BooksService.getAll().subscribe(result => this.books = result.sort(function (a, b) {
+        if (a.id! > b.id!) {
+          return -1;
+        }
+        if (a.id! < b.id!) {
+          return 1;
+        }
+        return 0;
+      }));
+
+    } else if (this.ordenarBy == 2) {
+
+      this.BooksService.getAll().subscribe(result => this.books = result.sort(function (a, b) {
+        if (a.num_pages! > b.num_pages!) {
+          return 1;
+        }
+        if (a.num_pages! < b.num_pages!) {
+          return -1;
+        }
+        return 0;
+      }));
+
+    }
+
   }
 
   
