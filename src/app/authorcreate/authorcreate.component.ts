780e420ce791581564fbdd151a3d6635200e528c
AlbertoJiLo <alberto_tgn_14@hotmail.com>
2022-12-19 19:41:42 +0100

    Formulario book y author

--- src/app/authorcreate/authorcreate.component.ts
+++ src/app/authorcreate/authorcreate.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorsService } from '../services/authors.service';
+import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
   selector: 'app-authorcreate',
   templateUrl: './authorcreate.component.html',
   styleUrls: ['./authorcreate.component.css']
 })
-export class AuthorcreateComponent {
+export class AuthorcreateComponent implements OnInit{
+
+  constructor (
+    private token: TokenStorageService, 
+    private authorsService: AuthorsService){}
+
+    user:any = null;
+    autor:any = {
+      name:null,
+      surname:null,
+      birth_date:null,
+      nationality:null,
+      descr:null,
+      gender:null
+    }
+    autores:any = [];
+    autorElegido:any = null;
+    creandoAutor: boolean = false;
+    creandoLibro: boolean = false;
+
+  ngOnInit(): void {
+
+    this.user = this.token.getUser();
+
+    this.authorsService.getAll().subscribe( result => this.autores = result);
+  }
+
+  iniciarFormulario(){
+
+    this.creandoAutor = !this.creandoAutor;
+
+  }
+
+  iniciarLibro(){
+
+    this.creandoLibro = !this.creandoLibro;
+
+  }
+
+  enviarDatos(){
+
+    window.alert("Autor "+this.autor.name+ " " +this.autor.surname+ " creado.")
+    console.log(this.autor)
+    this.creandoAutor = !this.creandoAutor;
+    
+    this.crearAutor();
+
+  }
+  crearAutor(){
+
+    this.authorsService.create(this.autor).subscribe();
+
+  }
+
 
 }
--- src/app/bookcreate/bookcreate.component.ts
+++ src/app/bookcreate/bookcreate.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { finalize } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { finalize, timeout } from 'rxjs';
+import { AuthorsService } from '../services/authors.service';
 import { BooksService } from '../services/books.service';
 import { EditorialsService } from '../services/editorials.service';
-import { AuthService } from '../_services/auth.service';
+import { WroteService } from '../services/wrote.service';
 import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
@@ -17,18 +18,24 @@ export class BookcreateComponent implements OnInit{
   constructor (
     private token: TokenStorageService, 
     private booksService: BooksService,
-    private auth: AuthService, 
-    private editorialsService: EditorialsService ){}
+    private editorialsService: EditorialsService,
+    private authorsService: AuthorsService,
+    private wroteService: WroteService ){}
 
+    @Input() autorElegido:any = null;
     editoriales:any = [];
-    editorial:any = {
-      id: null,
-      editorial_name: null
-    };
+
     
     editorialElegida:any = null;
     user:any = null;
+  
+
     letraDrawer:string = "";
+
+    libroCreado: any = {
+      id:null
+    };
+
     book:any = {
       isbn: null,
       title: null,
@@ -47,21 +54,71 @@ export class BookcreateComponent implements OnInit{
       }
     };
 
+    editorial:any = {
+      id: null,
+      editorial_name: null
+    };
+
+    autor:any={}
+
+    wrote:any = {
+      id_author:{
+        id:null
+      },
+      book:{
+        id:null
+      }
+    }
+
 
   ngOnInit(): void {
     
     this.user = this.token.getUser();
+    this.book.id_user.id=this.user.id;    
 
     this.editorialsService.getAll().subscribe( result => this.editoriales = result);
+
+    console.log(this.autorElegido)
 
   }
 
   enviarDatos(){
 
+    this.recogerEditorial();
+    this.recogerAutor();
+    this.recogerLibro();
+    //this.crearLibro();
+    //this.crearWrote();
 
-    this.editorialsService.getByName(this.editorialElegida).subscribe( result => this.editorial = result);
+  }
+
+  crearLibro(){
+
     this.book.id_editorial.id = this.editorial.id;
-    this.book.id_user.id=this.user.id;
+    this.booksService.create(this.book).pipe(finalize( () => this.crearWrote())).subscribe(result => this.libroCreado = result)
+
+  }
+
+
+  crearWrote(){
+
+
+    this.wrote.id_author.id = this.autor.id;
+    this.wrote.book.id = this.libroCreado.id;
+    this.wroteService.create(this.wrote).subscribe(res => console.log("Wrote añadido: "+res))
+    
+  }
+
+  recogerEditorial(){
+    this.editorialsService.getByName(this.editorialElegida).pipe(finalize( () => this.crearLibro()))
+    .subscribe(result => this.editorial = result);
+  }
+
+  recogerAutor(){
+    this.authorsService.getBySurname(this.autorElegido).subscribe(result => this.autor = result);
+  }
+
+  recogerLibro(){
 
     this.letraDrawer = this.book.title.substring(0,1);
     if(this.letraDrawer.search(/[a-zA-Z]/) == -1){
@@ -69,20 +126,6 @@ export class BookcreateComponent implements OnInit{
     }else{
       this.book.id_drawer.id=this.letraDrawer.toUpperCase();
     }
-
-    //this.sendRequests();
-    this.crearLibro();
-    
   }
 
-  crearLibro(){
-    this.booksService.create(this.book).subscribe();
-    console.log(this.book)
-  }
-
-  sendRequests(){
-
-    this.booksService.getByTitle(this.book.title).pipe(finalize( () => this.enviarDatos())).subscribe( result => this.book = result);
-    console.log(this.book)
-  }
 }
