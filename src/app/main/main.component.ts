050f7936795bb48f9c5553f212608d34968f1519
Carl0sLafu <114726441+Carl0sLafu@users.noreply.github.com>
2022-12-20 09:07:06 +0100

    Add files via upload

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -14,20 +14,35 @@ export class MainComponent {
   books: Books[] = [];
   count: number = 3;
   random: number = 0;
+  numrandomanterior: number = 0;
   libroAleatorio?: any ;
   generado: boolean = false;
 
   ngOnInit():void {
 
-    this.BooksService.getAll().subscribe(result => this.books = result);
+    this.BooksService.getAll().subscribe(result => { this.books = result.sort(function (a, b) {
+      if (a.id! > b.id!) {
+        return -1;
+      }
+      if (a.id! < b.id!) {
+        return 1;
+      }
+      return 0;
+    });}).closed;
 
   }
 
   generarLibroAleatorio(){
 
-    let numLibros = Math.floor(Math.random() * this.books.length);
-    this.random = numLibros;
-    this.libroAleatorio = this.books[this.random]
+    do {
+
+      let numLibros = Math.floor(Math.random() * this.books.length);
+      this.random = numLibros;
+
+    } while (this.numrandomanterior == this.random);
+
+    this.numrandomanterior = this.random;
+    this.libroAleatorio = this.books[this.random];
     this.generado = true;
 
   }
